635969f6ca9cf7f492d83905d84fc0c85278f2eb
LaoMuJi <992519078@qq.com>
2019-02-02 16:13:17 +0800

    1

--- planeWAR/1.py
+++ planeWAR/1.py
@@ -8,13 +8,39 @@ bg = pygame.image.load('./images/background.png')
 # 绘制图像
 screen.blit(bg,(0,0))
 
+
 # 英雄飞机
-hero_rect = pygame.Rect(100,500,120,125)
+hero = pygame.image.load('./images/me1.png')
+screen.blit(hero,(200,500))
+
+
+
+# 定义rect记录飞机的初始位置
+hero_rect = pygame.Rect(100,500,102,126)
 
 
 
 
 
+# 创建时钟对象
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(60)
+
+    # 捕获 事件监听
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
 
-pygame.quit()
+    hero_rect.y -= 1
+    if hero_rect.y < -126:
+        hero_rect.y = 700
+    #调用blit方法绘制图像
+    screen.blit(bg,(0,0))
+    screen.blit(hero,hero_rect)
+    # 更新屏幕显示
+    pygame.display.update()
+
