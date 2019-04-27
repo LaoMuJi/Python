56cde5de255cc7ea7499b7122efc844da4ff387c
LaoMuJi <992519078@qq.com>
2019-04-28 01:05:44 +0800

    .

--- py学习/03 多进程多线程协程/21.4进程池.py
+++ py学习/03 多进程多线程协程/21.4进程池.py
@@ -3,14 +3,14 @@ import time
 
 
 def worker(msg):
-    time.sleep(2)
+    time.sleep(5)
     print(msg, '执行完毕')
 
 
 if __name__ == '__main__':  # 必须有
 
-    po = multiprocessing.Pool(2)  # 定义一个进程池 最大
-    for i in range(0, 50):
+    po = multiprocessing.Pool(5)  # 定义一个进程池 最大
+    for i in range(0, 100):
         po.apply_async(worker, (i,))
 
     print('开始')
