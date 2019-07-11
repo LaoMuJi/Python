6612e70544ecef0ea28244132841f4525317fad9
LaoMuJi <992519078@qq.com>
2019-07-11 16:20:05 +0800

    .

--- test.py
+++ test.py
@@ -1,51 +1,42 @@
-import requests
-import json
-import time
+import re
 
 
-def a():
+def aa():
+    htmlstr = ''
+    html = '''
+    <br>
+    %s
+    <br>
+    <img src="%s" alt="" />
+    <br>
+    <audio src="%s" controls="controls"></audio>
+    <br>
+    %s
+    <br>
+    <br>
+    <br>
+    '''
 
-    loginurl = 'https://sx.byebyetext.com/api/app/users/third/login'
-    headers1 = {
-        "User-Agent": "okhttp/3.11.0",
-        'accept-encoding': 'gzip',
-        'content-length': '370',
-        'Host': 'sx.byebyetext.com',
-        'content-type': 'application/x-www-form-urlencoded'
-    }
-    data= 'openId=oR6sd1dTvATHpczuv2mpBicdnIOU&unionId=oGEegwG5z30-BQJQBXcSM-coEcNA&nickName=wxid_qfpud60bmqj421&avatarUrl=http%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FQ0j4TwGTfTLn2wBLGGSDR4Efhpb48XeTFUKCqicUddGGbUKBRdBZBOt7r8dcrXgSaNfdgxTuB1Le2J28XjZZZFQ%2F132&userGender=2&authType=1&countryCode=CN&appVersion=3.5.0&deviceInfo=unknown%2Funknown%2F8.1.0&platformId=1&deviceId='
-    rsp2 = requests.post(loginurl, headers=headers1, data=data)
-    json1 = json.loads(rsp2.content)
-    data_token = json1['data']
-    token = data_token['token']
-    b(token)
+    with open(r'C:\Users\lcc92\Desktop\音频.txt', 'r', encoding='UTF-8-sig') as f:
+        l = list(f)
+    print(l)
+    # for i in l:
+    #     m1 = re.match(r'^.*aac', i).group()
+    #     m2 = re.search(r' .*秒 ', i).group()
+    #     m3 = re.search(r'——.*——', i).group()
+    #     p = re.compile(r'——')
+    #     m3 = p.sub(r'', m3)
+    #     try:
+    #         m4 = re.search(r'http://im.*jpeg', i).group()
+    #     except:
+    #         m4 = ''
+    #     htmll = html % (m3, m4, m1, m2)
+    #     htmlstr += htmll
+    #     # print(htmlstr)
+    #
+    #
+    # with open(r'C:\Users\lcc92\Desktop\1.html', 'r+', encoding='utf-8') as f:
+    #     f.seek(285, 0)
+    #     f.write(htmlstr)
 
-
-def b(token):
-
-    url = "https://sx.byebyetext.com/api/app/users/6301/voices?"
-    kw = {
-        "lastId": "null",
-        'moduleId': '2'
-    }
-    headers2 = {
-        "User-Agent": "okhttp/3.11.0",
-        'accept-encoding': 'gzip',
-        'authorization': 'Bearer '+token,
-        'Host': 'sx.byebyetext.com',
-        'content-type': 'application/x-www-form-urlencoded'
-    }
-    rsp = requests.get(url, params=kw, headers=headers2)
-    a = rsp.content.decode("raw_unicode_escape")
-    b = json.loads(a)
-    c = b['data']
-    # print(c)
-    for d in c:
-        e = time.localtime(int(d['created_at']))
-        f = time.strftime("%Y-%m-%d %H:%M:%S", e)
-
-        print(d['voice_url'],f,d['voice_len']+'秒',d['topic_name'])
-
-if __name__ == '__main__':
-    a()
-
+aa()
